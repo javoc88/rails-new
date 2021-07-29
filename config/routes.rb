2f673c91772b780005c097afac6d85215750a260
Javier Ochoa <javier.ochoa.77@gmail.com>
2021-07-29 11:34:23 -0400

    terminado

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'index', to: 'tasks#index'
-  get "tasks/new", to: "tasks#new", as: :new_task
-  get "tasks/:id", to: "tasks#show", as: :task
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  patch "tasks/:id", to: "tasks#update"
-  post "tasks", to: "tasks#create"
+  root to: 'tasks#index'
+  resources :tasks
 end
